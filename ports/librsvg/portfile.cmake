074242b7e93f2356152c3965781b25c3c39b7af4
Toshiyuki Ogawa <ogawa@oh-laser.com>
2020-05-10 14:36:39 +0900

    I had the librsvg-2.48.
    To build librsvg-2.48, you have install rust compiler.
    I did not check for not windows environment.
    
    It may be not good to install rust compiler individually.

--- ports/librsvg/portfile.cmake
+++ ports/librsvg/portfile.cmake
@@ -28,7 +28,7 @@ vcpkg_fixup_cmake_targets(CONFIG_PATH share/unofficial-librsvg TARGET_PATH share
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
 
 # Handle copyright
-file(INSTALL ${SOURCE_PATH}/COPYING DESTINATION ${CURRENT_PACKAGES_DIR}/share/librsvg RENAME copyright)
+file(INSTALL ${SOURCE_PATH}/COPYING.LIB DESTINATION ${CURRENT_PACKAGES_DIR}/share/librsvg RENAME copyright)
 
 vcpkg_copy_pdbs()
 
